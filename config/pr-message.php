0b29f3266ca528adc90893504b19ddb3fe408238
naotty <teruya.nao@gmail.com>
2025-05-07 15:51:22 +0900

    misc

--- config/pr-message.php
+++ config/pr-message.php
@@ -11,7 +11,7 @@ return [
     */
     'messages' => [
         'Would you like to work with us? We are hiring engineers!',
-        'Let\'s create better services together',
+        'Let\'s create better services together!',
         'Would you like to grow with us?',
         'Looking for a challenging environment? Join us!',
     ],
